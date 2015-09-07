6608364dd307409779da46544168a00247e3ff06
mingyuan huang <mh2239@cornell.edu>
2015-09-06 22:50:36 -0400

    Evaluate tree visitor

--- project1/src/cs4321/project1/EvaluateTreeVisitor.java
+++ project1/src/cs4321/project1/EvaluateTreeVisitor.java
@@ -1,10 +1,13 @@
 package cs4321.project1;
 
+import java.util.Stack;
+
 import cs4321.project1.tree.DivisionTreeNode;
 import cs4321.project1.tree.LeafTreeNode;
 import cs4321.project1.tree.SubtractionTreeNode;
 import cs4321.project1.tree.AdditionTreeNode;
 import cs4321.project1.tree.MultiplicationTreeNode;
+import cs4321.project1.tree.TreeNode;
 import cs4321.project1.tree.UnaryMinusTreeNode;
 
 /**
@@ -14,43 +17,80 @@ import cs4321.project1.tree.UnaryMinusTreeNode;
  */
 
 public class EvaluateTreeVisitor implements TreeVisitor {
-
+	Stack<Double> stack ;
 	public EvaluateTreeVisitor() {
 		// TODO fill me in
+		 stack = new Stack<Double>();
 	}
 
 	public double getResult() {
 		// TODO fill me in
-		return 42; // so that skeleton code compiles
+		
+		return stack.pop(); // so that skeleton code compiles
 	}
 
 	@Override
 	public void visit(LeafTreeNode node) {
 		// TODO fill me in
+		this.stack.push(node.getData());
 	}
 
 	@Override
 	public void visit(UnaryMinusTreeNode node) {
 		// TODO fill me in
+		node.getChild().accept(this);
+		double num = stack.pop();
+		stack.push(-num);
+		
+		
 	}
 
 	@Override
 	public void visit(AdditionTreeNode node) {
 		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		double num2 = stack.pop();
+		double num1 = stack.pop();
+		num1 = num1 + num2;
+		stack.push(num1);
+		
+		
+		
 	}
 
 	@Override
 	public void visit(MultiplicationTreeNode node) {
 		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		double num1 = stack.pop();
+		double num2 = stack.pop();
+		num1 = num1 * num2;
+		stack.push(num1);
+		
+		
 	}
 
 	@Override
 	public void visit(SubtractionTreeNode node) {
 		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		double num2 = stack.pop();
+		double num1 = stack.pop();
+		num1 = num1 - num2;
+		stack.push(num1);
 	}
 
 	@Override
 	public void visit(DivisionTreeNode node) {
 		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		double num2 = stack.pop();
+		double num1 = stack.pop();
+		num1 = num1 / num2;
+		stack.push(num1);	
 	}
 }
