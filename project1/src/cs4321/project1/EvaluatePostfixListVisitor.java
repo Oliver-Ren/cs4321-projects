a001f7d479877484d515475c67a4ca289602c671
mingyuan huang <mh2239@cornell.edu>
2015-09-07 20:34:57 -0400

    Evaluate postfix list visitor done

--- project1/src/cs4321/project1/EvaluatePostfixListVisitor.java
+++ project1/src/cs4321/project1/EvaluatePostfixListVisitor.java
@@ -1,5 +1,7 @@
 package cs4321.project1;
 
+import java.util.Stack;
+
 import cs4321.project1.list.DivisionListNode;
 import cs4321.project1.list.SubtractionListNode;
 import cs4321.project1.list.NumberListNode;
@@ -13,44 +15,112 @@ import cs4321.project1.list.UnaryMinusListNode;
  * @author Your names and netids go here
  */
 public class EvaluatePostfixListVisitor implements ListVisitor {
-
+	Stack<Double> stack;
 	public EvaluatePostfixListVisitor() {
 		// TODO fill me in
+		stack = new Stack<Double>();
+		
 	}
 
 	public double getResult() {
 		// TODO fill me in
-		return 42; // so that skeleton code compiles
+		
+		return stack.pop(); // so that skeleton code compiles
 	}
 
 	@Override
 	public void visit(NumberListNode node) {
 		// TODO fill me in
+		if(node.getNext()!=null) {
+			stack.push(node.getData());
+			node.getNext().accept(this);
+		} else {
+			stack.push(node.getData());
+		}  // A: maybe not needed, since the last node should always be an operand
+		
 	}
 
 	@Override
 	public void visit(AdditionListNode node) {
 		// TODO fill me in
+		if(node.getNext()!=null) {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first + second;
+			stack.push(first);
+			node.getNext().accept(this);
+		} else {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first + second;
+			stack.push(first);
+		}
 	}
 
 	@Override
 	public void visit(SubtractionListNode node) {
 		// TODO fill me in
+		if(node.getNext()!=null) {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first - second;
+			stack.push(first);
+			node.getNext().accept(this);
+		} else {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first - second;
+			stack.push(first);
+		}
 	}
 
 	@Override
 	public void visit(MultiplicationListNode node) {
 		// TODO fill me in
+		if(node.getNext()!=null) {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first * second;
+			stack.push(first);
+			node.getNext().accept(this);
+		} else {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first * second;
+			stack.push(first);
+		}
 	}
 
 	@Override
 	public void visit(DivisionListNode node) {
 		// TODO fill me in
+		if(node.getNext()!=null) {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first / second;
+			stack.push(first);
+			node.getNext().accept(this);
+		} else {
+			double second = stack.pop();
+			double first = stack.pop();
+			first = first / second;
+			stack.push(first);
+		}
 	}
 
 	@Override
 	public void visit(UnaryMinusListNode node) {
 		// TODO fill me in
+		if(node.getNext()!=null) {
+			double first = stack.pop();
+			first = -first ;
+			stack.push(first);
+			node.getNext().accept(this);
+		} else {
+			double first = stack.pop();
+			first = -first;
+			stack.push(first);
+		}
 	}
 
 }
