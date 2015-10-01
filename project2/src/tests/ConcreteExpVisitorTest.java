ce61f4c2f43934181ddc9467dd0838520364039b
Chengxiang Ren <cr486@cornell.edu>
2015-09-30 22:14:01 -0400

    paritially completed visitor

---
+++ project2/src/tests/ConcreteExpVisitorTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConcreteExpVisitorTest {
+
+
+	@Test
+	public void testVisitEqualsTo() {
+		
+	}
+
+}
--- project2/src/visitors/ConcreteExpVisitor.java
+++ project2/src/visitors/ConcreteExpVisitor.java
@@ -11,11 +11,29 @@ import net.sf.jsqlparser.expression.operators.relational.NotEqualsTo;
 import net.sf.jsqlparser.schema.Column;
 
 public class ConcreteExpVisitor extends AbstractExpVisitor {
-
+	// the current numeric value holding in the visitor.
+	private long currNumericValue = 0;
+	// the current condition holding in the visitor.
+	private boolean currCondition = false;
+	
+	/**
+	 * Returns the final condition of the visitor.
+	 * @return the current condition.
+	 */
+	public boolean getFinalCondition() {
+		return currCondition;
+	}
+	
+	/**
+	 * Visit the expression of type LongValue.
+	 * As LongValue is a leaf expression, set
+	 * the the value of expression to current numeric value.
+	 * 
+	 * @param arg0 the expression of type LongValue.
+	 */
 	@Override
 	public void visit(LongValue arg0) {
-		// TODO Auto-generated method stub
-		arg0.getValue();
+		currNumericValue = arg0.getValue();
 	}
 
 	@Override
@@ -30,12 +48,28 @@ public class ConcreteExpVisitor extends AbstractExpVisitor {
 		
 	}
 
+	/**
+	 * Visit the expression of type LongValue.
+	 * After evaluating its left child, there should be a valid numeric
+	 * value produced by the left child expression. And so for the right child.
+	 * The condition the visitor holds after evaluating this expression should 
+	 * be (leftValue == rightValue).
+	 * 
+	 * @param arg0 the expression of type EqualsTo.
+	 */
 	@Override
 	public void visit(EqualsTo arg0) {
-		// TODO Auto-generated method stub
-		
+		arg0.getLeftExpression().accept(this);
+		long leftValue = currNumericValue;
+		arg0.getRightExpression().accept(this);
+		long rightValue = currNumericValue;
+		// update the condition.
+		currCondition = (leftValue == rightValue);		
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void visit(NotEqualsTo arg0) {
 		// TODO Auto-generated method stub
