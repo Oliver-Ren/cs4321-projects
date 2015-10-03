75e70b3cbc2e1c5bb7975e87df068587317e216b
Chengxiang Ren <cr486@cornell.edu>
2015-10-02 23:19:01 -0400

    expected

---
+++ project2/sqltests/distinct/input/queries.sql
@@ -0,0 +1,5 @@
+select DISTINCT * from Sailors;
+select DISTINCT B from Sailors;
+select DISTINCT C FROM Sailors order by C;
+select DISTINCT * from Sailors S1, Sailors S2, Reserves R;
+select distinct * from Sailors S1, Sailors S2, Reserves R order by S2.A;
