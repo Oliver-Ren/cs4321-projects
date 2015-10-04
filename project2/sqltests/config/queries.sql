5b11708001b968dccc38840f8aa0bf35edbff6be
Chengxiang Ren <cr486@cornell.edu>
2015-10-03 20:11:34 -0400

    added more tests

--- project2/sqltests/config/queries.sql
+++ project2/sqltests/config/queries.sql
@@ -1,1 +1,5 @@
-select distinct * from Sailors S1, Sailors S2, Reserves R order by S2.A, R.H;
+SELECT * FROM Reserves WHERE Reserves.G > 1;
+SELECT * FROM Sailors where Sailors.A < 4 and Sailors.B >= 102;
+SELECT * FROM Boats where Boats.E = 3 and 1 < 0;
+SELECT * FROM Boats where 2 > Boats.E and 1 > 0;
+SELECT * FROM Sailors where 4 > Sailors.A and 102 <= Sailors.B;
