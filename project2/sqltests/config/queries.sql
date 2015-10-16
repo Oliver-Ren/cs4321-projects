a8ed5da303bd5e3a6550be1ee0fe989f84c5e517
mingyuan huang <mh2239@cornell.edu>
2015-10-16 16:04:37 -0400

    changed a query

--- project2/sqltests/config/queries.sql
+++ project2/sqltests/config/queries.sql
@@ -1,5 +1,1 @@
-SELECT * FROM Reserves WHERE Reserves.G > 1;
-SELECT * FROM Sailors where Sailors.A < 4 and Sailors.B >= 102;
-SELECT * FROM Boats where Boats.E = 3 and 1 < 0;
-SELECT * FROM Boats where 2 > Boats.E and 1 > 0;
-SELECT * FROM Sailors where 4 > Sailors.A and 102 <= Sailors.B;
+SELECT * FROM Boats B1, Boats B2, Boats B3 where 1 = 1; 
