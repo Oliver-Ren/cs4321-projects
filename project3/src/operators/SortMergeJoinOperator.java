94f3661ff69e648fc2aa505e5f0b2808f4a24a2b
gz94 <gz94@cornell.edu>
2015-10-31 03:42:01 -0400

    updates

--- project3/src/operators/SortMergeJoinOperator.java
+++ project3/src/operators/SortMergeJoinOperator.java
@@ -18,13 +18,9 @@ import nio.TupleReader;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 
 public class SortMergeJoinOperator extends JoinOperator {
-	//int cnt = 0;// debug perpiose
 	int partitionIndex = 0;  // the index of the first tuple in the current partition
 	int curRightIndex = 0;  // the index of the current tuple of left table
-	//TupleReader leftReader; // the tuple reader for left table 
-	//TupleReader rightReader; //the  tuple reader for the right table
 	Expression exp;  // my expression
-	//List<EqualsTo> equalExps; 
 	JoinExpVisitor jv;
 	
 	Tuple leftTp;
@@ -32,50 +28,11 @@ public class SortMergeJoinOperator extends JoinOperator {
 	
 	TupleComp cp = null;
 	
-	// = =============some problems
-	List<Integer> leftOrders = null; // the order of attributes in left table 
-	List<Integer> rightOrders = null;// the order of attributes in right table 
-	/**
-	 * scan a list of expressions and keep the equals expression
-	 * 
-	 * @return
-	 * @throws IOException 
-	 */
-//	public List<EqualsTo> getEqualExps(List<Expression> exp){
-//		if(exp == null) return null;
-//		for(Expression e : exp ){
-//			if(e instanceof EqualsTo){
-//				EqualsTo eq = (EqualsTo) e;
-//				equalExps.add(eq);
-//			}
-//		}
-//		return equalExps;
-//		
-//	}
-	public void dumpLeft() {
-		BinaryTupleWriter tw;
-		try{
-			tw  = new BinaryTupleWriter("benchmarking/samples/temp/dumpleft");
-			Tuple t;
-			while((t = left.getNextTuple())!=null){
-				
-				tw.write(t);
-			}
-			
-			tw.close();
-			FormatConverter.binToNormal("benchmarking/samples/temp/dumpleft", 
-					"benchmarking/samples/temp/dumpRight_humanreadable");
-			SortOperator sp = (SortOperator)left;
-			sp.reset(0);
-		}catch(IOException e){
-			e.printStackTrace();
-			return;
-		}
-		
-		
-	}
-	//@Override
-	public Tuple getNextTuple2()  {		
+	List<Integer> leftOrders = null; // the order of attributes in left table
+	List<Integer> rightOrders = null;// the order of attributes in right table
+	
+	@Override
+	public Tuple getNextTuple()  {
 		while(leftTp !=null && rightTp !=null){
 			if (cp.compare(leftTp, rightTp) < 0) {
 				leftTp = left.getNextTuple();
@@ -95,11 +52,15 @@ public class SortMergeJoinOperator extends JoinOperator {
 			}			
 			rightTp = right.getNextTuple();
 			curRightIndex++;
+			
 			if (rightTp == null || 
 					cp.compare(leftTp, rightTp) != 0) {
+				// System.out.println("cur: " + leftTp.toString());
 				leftTp = left.getNextTuple();
+				// System.out.println(leftTp);
 				((SortOperator) right).reset(partitionIndex);
 				curRightIndex = partitionIndex;
+				rightTp = right.getNextTuple();
 			}
 			
 			if (rst != null) return rst;
@@ -108,12 +69,7 @@ public class SortMergeJoinOperator extends JoinOperator {
 		return null;
 	}
 	
-	@Override
-	public Tuple getNextTuple()  {		
-		
-		
-		
-		
+	public Tuple getNextTuple2()  {		
 		while(leftTp !=null && rightTp !=null){ //当两个table都没EOF
 			if(cp.compare(leftTp, rightTp)!=0){
 				while (leftTp != null && 
@@ -182,19 +138,11 @@ public class SortMergeJoinOperator extends JoinOperator {
 		
 		System.out.println("------------");
 		
-		//EqualsTo et = (EqualsTo) exp;
-		//left.schema().contains(et.getLeftExpression());
-		// decompose all the end expression to a list
-		//equalExps = getEqualExps(list); // assign the equalExpression to my list
 		this.jv = new JoinExpVisitor(left.schema(),right.schema());
 		leftTp = left.getNextTuple();
 		rightTp = right.getNextTuple();
 		
-		
 		System.out.println("I'm in sort merge join");
-		
-		
-		
 	}
 	
 	// comparator class to compare one tuple from the left table and another from the 
@@ -204,25 +152,20 @@ public class SortMergeJoinOperator extends JoinOperator {
 		List<Integer> rightOrders = null;// the order of attributes in right table 
 		@Override
 		public int compare(Tuple left, Tuple right) {
-			
-			
 			for( int i = 0; i< leftOrders.size();i++){
 				int leftVal = left.cols[leftOrders.get(i)];
 				int rightVal = right.cols[rightOrders.get(i)];
 				int cmp = Integer.compare(leftVal, rightVal);
 				if(cmp != 0) return cmp;		
 			}
+            
 			return 0;
-				
-			
 		}
 		
 		public TupleComp(List<Integer> leftOrders, List<Integer> rightOrders){
 				this.leftOrders = leftOrders;
 				this.rightOrders = rightOrders;
-				
 		}
-		
 	}
 
 	@Override
