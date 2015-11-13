c105f37c35b1be977b7136a7ec9ddbf287ef020f
mingyuan huang <mh2239@cornell.edu>
2015-11-13 13:41:42 -0500

    updated

---
+++ project4/src/tests/Project4Test.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Project4Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
