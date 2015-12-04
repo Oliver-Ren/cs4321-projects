4cfa1d811ffcb1b605860914b2be3f62e082acdb
Chengxiang Ren <cr486@cornell.edu>
2015-12-04 15:55:30 -0500

    selection optimizer

---
+++ project5/src/optimizer/SelectionOptimizer.java
@@ -0,0 +1,14 @@
+package optimizer;
+
+import net.sf.jsqlparser.expression.Expression;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import util.IndexInfo;
+
+
+public class SelectionOptimizer {
+	
+	public static IndexInfo whichIndexToUse(String tableName, Expression exp) {
+		//TODO
+		throw new NotImplementedException();
+	}
+}
