ca47966a1f940334eb045684f1597da7a26b65a7
gz94 <gz94@cornell.edu>
2015-12-04 01:19:53 -0500

    multi join

---
+++ project5/src/operators/logic/LogicMultiJoinOp.java
@@ -0,0 +1,14 @@
+package operators.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogicMultiJoinOp {
+
+	public List<String> froms = new ArrayList<>();
+	
+	public LogicMultiJoinOp(List<String> froms) {
+		this.froms = froms;
+	}
+	
+}
